8b9cce662945c2db9f3047bc3700469f2f3ae859
acordafrederick <sonjaire@gmail.com>
2018-04-03 09:18:13 -0500

    heroku changes

--- server.js
+++ server.js
@@ -1,98 +1,32 @@
-// Include Server Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Require History Schema
-var History = require("./models/History");
-
-// Create Instance of Express
-var app = express();
-// Sets an initial port. We'll use this later in our listener
-var PORT = process.env.PORT || 3000;
-
-// Run Morgan for Logging
-app.use(logger("dev"));
-app.use(bodyParser.json());
+// Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-app.use(express.static("public"));
+// Add routes, both API and view
+app.use(routes);
 
-// -------------------------------------------------
+// Set up promises with mongoose
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/nytreact",
+  process.env.MONGODB_URI || "mongodb://localhost/nytarticles",
   {
+    useMongoClient: true
   }
 );
-// MongoDB Configuration configuration (Change this URL to your own DB)
-// mongoose.connect("mongodb://heroku_wsl4kwct:1tdue2s77s4noj3hhp6d2sprdk@ds119585.mlab.com:19585/heroku_wsl4kwct");
-var db = mongoose.connection;
 
-db.on("error", function(err) {
-  console.log("Mongoose Error: ", err);
-});
-
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
-});
-
-// -------------------------------------------------
-
-// Main "/" Route. This will redirect the user to our rendered React application
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-// This is the route we will send GET requests to retrieve our most recent search data.
-// We will call this route the moment our page gets rendered
-app.get("/api", function(req, res) {
-
-  // We will find all the records, sort it in descending order, then limit the records to 5
-  History.find({}).sort([
-    ["dateSaved", "descending"]
-  ]).limit(5).exec(function(err, doc) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      res.send(doc);
-    }
-  });
-});
-
-// This is the route we will send POST requests to save each search.
-app.post("/api", function(req, res) {
-  console.log("here");
-  console.log("BODY: " + req.body.topic);
-
-  // Here we'll save the article based on the JSON input.
-  // We'll use Date.now() to always get the current date time
-  var history = new History({
-    topic: req.body.topic,
-    date: req.body.date,
-    url: req.body.url,
-    dateSaved: Date.now()
-  });
-
-  history.save(function(err, doc) {
-        if(err) { 
-          console.log(err);
-        } else {
-          res.send("Saved Search");
-        }
-
-    });
-  
-});
-
-// -------------------------------------------------
-
-// Listener
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
